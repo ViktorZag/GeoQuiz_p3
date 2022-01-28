edd3c4fe286b3977ced76e0c285174e32f6ed6b4
Viktor <viktorzahurskiy@gmail.com>
2022-01-28 20:39:05 +0200

    develop-3

--- app/src/main/java/com/viktor_zet/geoquiz_p3/MainActivity.kt
+++ app/src/main/java/com/viktor_zet/geoquiz_p3/MainActivity.kt
@@ -2,10 +2,13 @@ package com.viktor_zet.geoquiz_p3
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : Activity() {
 
@@ -26,6 +29,7 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -34,20 +38,43 @@ class MainActivity : Activity() {
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
-           checkAnswer(true)
+            checkAnswer(true)
         }
         falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
         }
-
         nextButton.setOnClickListener {
             currentIndex = currentIndex + 1
             if (currentIndex == questionList.size) currentIndex = 0
             updateQuestion()
         }
-
         updateQuestion()
+    }
 
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun updateQuestion() {
@@ -64,4 +91,6 @@ class MainActivity : Activity() {
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
+
+
 }
